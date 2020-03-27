4272d61684f6c82de9b1c806403c8143e53e2f47
newnag <48855584+newnag@users.noreply.github.com>
2020-03-27 17:29:05 +0700

    create

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/style.css">
+</head>
+<body>
+    
+<header>
+    <div class="header">
+        <div class="logo">
+            <figure><img src="<?php bloginfo('template_url') ?>/img/logo/logo.png" alt=""></figure>
+        </div>
+
+        <div class="text-head">
+            <h1>Tutor Web Test Exam</h1>
+        </div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<main>
+    <article>
+        <div class="content container">
+            <p>Test</p>
+        </div>
+    </article>
+</main>
+
+</body>
+</html>
